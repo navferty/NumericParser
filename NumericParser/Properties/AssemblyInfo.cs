92afda61748ebe19da0759010a60b70320cccab9
Andrey Deyev <Andrey.Deyev@eycistt.local>
2018-05-13 14:08:31 +0300

    Create repo NumericParser

---
+++ NumericParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NumericParser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EY")]
+[assembly: AssemblyProduct("NumericParser")]
+[assembly: AssemblyCopyright("Copyright © EY 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c38643c-0812-49fc-9e1c-066df80928f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NumericParserTests/DecimalParserTests.cs
@@ -0,0 +1,30 @@
+﻿using NumericParser;
+using NUnit.Framework;
+
+namespace NumericParserTests
+{
+	[TestFixture(Category = "Unit")]
+	public class NumericParserTests
+	{
+		[TestCase("0",               0)]
+		[TestCase("123",             123)]
+		[TestCase("1.1",             1.1)]
+		[TestCase("1,1",             1.1)]
+		[TestCase("1 000",           1000)]
+		[TestCase("12  00 00 . 12",  120000.12)]
+		[TestCase("0.1",             0.1)]
+		[TestCase(".123",            0.123)]
+		[TestCase("10.000.000",      10000000)]
+		[TestCase("10.000,12",       10000.12)]
+		[TestCase("12,345.12",       12345.12)]
+		[TestCase("12,345,000",      12345000)]
+		[TestCase("1E6",             1000000)]
+		[TestCase("1.2E3",           1200)]
+		[TestCase("-1.3E-5",         -0.000013)]
+		[TestCase("no value",        null)]
+		public void ParseDecimal(string sourceValue, decimal? targetValue)
+		{
+			Assert.AreEqual(targetValue, sourceValue.ParseDecimal());
+		}
+	}
+}
