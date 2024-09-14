135d76b0325ea8240f4d275970aceb522d4cf1c5
navferty <navferty@ymail.com>
2024-09-15 01:38:56 +0400

    Add separate benchmark project for .net framework 4.8.1

---
+++ NumericParser.Benchmarks.NetFramework481/NumericParserBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace NumericParser.Benchmarks.NetFramework481
+{
+	[MemoryDiagnoser, ShortRunJob]
+	public class NumericParserBenchmarks
+	{
+		[Params(
+			"1",
+			"11",
+			"111",
+			"1111",
+			"11111",
+			"12,91",
+			"34.56",
+			"12.345.678,91",
+			"12,345,678.91",
+			"-1 000 000 . 321",
+			"1234567.89",
+			".123456789",
+			"123.456.789",
+			"15E3",
+			"-5.371E8")]
+		public string Input { get; set; }
+
+		[Benchmark]
+		public decimal ParseDecimal()
+		{
+			return Input.ParseDecimal();
+		}
+	}
+}
