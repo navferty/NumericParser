aeb7c7d73798df4b2d6f521fceb4e6e6cfc82dad
navferty <navferty@ymail.com>
2024-09-28 14:20:44 +0400

    #12 Adjust exceptions thrown by ParseDecimal
    Enhanced ParseDecimal method:
    - Added null check for value parameter, throwing ArgumentNullException.
    - Changed exception from ArgumentException to FormatException.
    - OverflowException exception can not be thrown now: decimal oveflow is managed internally in decimal.Parse (see Number.Parsing.cs `TryNumberToDecimal` method in dotnet source code).
    
    Updated DecimalParserSettings:
    - Added Default property for default instance initialization.

--- NumericParser/DecimalParserSettings.cs
+++ NumericParser/DecimalParserSettings.cs
@@ -2,6 +2,8 @@
 
 public sealed class DecimalParserSettings
 {
+	public static DecimalParserSettings Default { get; } = new();
+
 	/// <summary>
 	/// Prefer thousands in ambiguous cases, when it cannot be determined, for example
 	/// <code>123,456</code>. Set to true if such input should be treated as "123 thousands and 456".
