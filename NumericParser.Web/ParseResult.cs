c56cfddf4f04cf5396bcbcdc132d6f484faa35d8
navferty <navferty@ymail.com>
2024-09-28 15:22:24 +0400

    Add ASP.NET Core Web API project to parse single or batch decimal value

---
+++ NumericParser.Web/ParseResult.cs
@@ -0,0 +1,8 @@
+﻿namespace NumericParser.Web;
+
+public sealed record ParseResult
+{
+	public required string Source { get; init; }
+	public decimal? Value { get; init; }
+	public string? Error { get; init; }
+}
